931a457604212b1b3b12716711a6e64ced57e84c
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2019-05-14 11:20:32 +0530

    Add schema to graphQL

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+consr schema = require('./schema/schema');
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({}));
+app.use("/graphql", graphqlHTTP({
+	schema
+}));
 
 app.listen(4000, () => {
 	console.log("Now Listing for requests on port 4000");
---
+++ server/schema/schema.js
@@ -0,0 +1,29 @@
+const graphql = require("graphql");
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+const BookType = new GraphQLObjectType({
+	name: "Book",
+	fields: () => ({
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		genre: { type: GraphQLString }
+	})
+});
+
+const RootQuery = new GraphQLObjectType({
+	name: "RootQueryType",
+	fields: {
+		book: {
+			type: BookType,
+			args: { id: { type: GraphQLString } },
+			resolve(parent, arg) {
+				// Code to get data from db / other source
+			}
+		}
+	}
+});
+
+module.exports = new GraphQLSchema({
+	query: RootQuery
+});
