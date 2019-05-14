9578749da262bb90f6376afba58b5e0924dd1307
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2019-05-14 11:07:31 +0530

    Setup graphQL

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
 
 const app = express();
+
+app.use("/graphql", graphqlHTTP({}));
 
 app.listen(4000, () => {
 	console.log("Now Listing for requests on port 4000");
