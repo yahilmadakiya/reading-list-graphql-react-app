8e43f54cfa361cf13c84ecd800a93687607eb001
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2019-05-28 12:03:40 +0530

    Remove dummy data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -12,22 +12,6 @@ const {
 	GraphQLList
 } = graphql;
 
-// dummy data
-var books = [
-	{ name: "Name of the Wind", genre: "Fantasy", id: "1", authorid: "1" },
-	{ name: "The Final Empire", genre: "Fantasy", id: "2", authorid: "2" },
-	{ name: "The Long Earth", genre: "Sci-Fi", id: "3", authorid: "3" },
-	{ name: "The Hero of Ages", genre: "Fantasy", id: "4", authorid: "2" },
-	{ name: "The Colour of Magic", genre: "Fantasy", id: "5", authorid: "3" },
-	{ name: "The Light Fantastic", genre: "Fantasy", id: "6", authorid: "3" }
-];
-
-var authors = [
-	{ name: "patrick Rothfuss", age: "44", id: "1" },
-	{ name: "Brandon Sanderson", age: "42", id: "2" },
-	{ name: "Terry Pratchett", age: "66", id: "3" }
-];
-
 const BookType = new GraphQLObjectType({
 	name: "Book",
 	fields: () => ({
