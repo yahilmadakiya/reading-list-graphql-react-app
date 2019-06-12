6af0b75405e3ac10e655df41ffaf6687741b727f
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2019-06-12 14:49:51 +0530

    Apollo Client Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
 // Components
 import BookList from "./component/BookList";
 
+// apollo client setup
+const client = new ApolloClient({
+	uri: "http://localhost:4000/graphql"
+});
+
 function App() {
 	return (
-		<div className="main">
-			<h1>Ninja's Reading List</h1>
-			<BookList />
-		</div>
+		<ApolloProvider client={client}>
+			<div className="main">
+				<h1>Ninja's Reading List</h1>
+				<BookList />
+			</div>
+		</ApolloProvider>
 	);
 }
 
